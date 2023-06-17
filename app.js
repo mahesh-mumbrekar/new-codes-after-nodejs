e656daf58071331758e34df6bd7fe8cf07aff850
kwingston <mahesh.mummbrekar@gmail.com>
2023-06-17 23:50:34 +0530

    created function for the server to response after getting requests

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 
 const http = require('http');
 
+
+function handleRequest(request, response) {
+    response.statusCode = 200;
+    response.end('<h1> Hello World</h1>')
+    
+}
+
 const server = http.createServer();
 
 
