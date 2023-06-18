dbaf1d058cee337b4bc5d5431edfe0a23eb79a6b
kwingston <mahesh.mummbrekar@gmail.com>
2023-06-18 18:15:16 +0530

    added some extra code in the fuction to handle request for current time

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const http = require('http');
 
 
 function handleRequest(request, response) {
-    response.statusCode = 200;
-    response.end('<h1> Hello World</h1>')
+
+    if (request.url === '/currenttime') {
+     response.statusCode = 200;
+     response.end('<h1>'+new Date().toISOString() + '<h1>')
+    } else if(request.url==='/'){
+     response.statusCode = 200;
+     response.end('<h1> Hello World</h1>')
+    }
+  
     
 }
 
-const server = http.createServer();
+const server = http.createServer(handleRequest);
 
 
 server.listen(3000);
