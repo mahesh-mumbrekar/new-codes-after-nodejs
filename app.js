c0c2962cfcb0b30758a44eb638e5b5004e59b4ab
kwingston <mahesh.mummbrekar@gmail.com>
2023-06-19 19:31:10 +0530

    created new server form which takes request of username value and and stores the value and give response  that username stored

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const express= require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/currenttime', function (req, res) {
     res.send('<h1>' + new Date().toISOString() + '</h1>');
 })
 
 app.get('/', function (req, res) {
-    res.send('<h1>Hello world!</h1>');
+    res.send('<form action="/store-user" method="POST" ><label>Your Name</label><input type="text" name="userName"><button>submit</button></form>');
+})
+
+app.post('/store-user', function(req, res){
+    const userName = req.body.userName; 
+    console.log(userName);
+    res.send('<h1>UserName Stored!</h1>')
+
 })
 
 app.listen(3000);
